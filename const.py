d2b738831cd8fe2e52ee602d7bc6781dfe88e20a
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2024-04-22 12:20:24 +0300

    Update const.py

--- const.py
+++ const.py
@@ -16,8 +16,7 @@ RANDOM_TEXT_PART_1 = [f'Тебе выпала', 'Звёзды подсказыв
                       'Тебе может понравиться']
 OLYMP_TEXT_PART_1 = ['Я нашел олимпиаду, которая тебе понравится', 'Тебе подходит', "Эта олимпиада тебе подходит"]
 
-TOKEN = '6299843038:AAEF_P9M-J5LipQhkODdrZ13DKRcf-ZQ2fY' # navigator
-# TOKEN = '5436835423:AAFrLWVJWu0zZSTbp04UIJ2Ooqjxgw1bzI8'  # foxbot
+TOKEN = TOKEN
 
 OLYMPIADS = pd.read_csv("data/olympiads_23_24.csv", on_bad_lines='skip')
 RANDOM_PROF = {
